3d4f8705a18484c896333e2333a61a8d5d33eb83
OLMI <OLMI@DESKTOP-QSSM5M1>
2023-09-02 16:36:38 +0300

    Добавьте файлы проекта.

---
+++ Dice Game Simulator/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Emit;
+
+namespace DiceGameSimulator
+{
+    internal class Game
+    {
+        public void Start()
+        {
+            Label:
+            Console.WriteLine("╔══════════════════════════════════╗");
+            Console.WriteLine("╟ Введите 'старт', для начала игры ╢");
+            Console.WriteLine("╚══════════════════════════════════╝");
+            string starting = Console.ReadLine();
+            while (starting != "старт")
+            {
+                Console.WriteLine("╔════════╗");
+                Console.WriteLine("╟ Ошибка ╢");
+                Console.WriteLine("╚════════╝");
+                goto Label;
+            }
+            if (starting == "старт")
+            {
+                Random random = new Random();
+                int firstValue = random.Next(6);
+                int secondValue = random.Next(6);
+                Console.WriteLine("╔═════════════════════╗");
+                Console.WriteLine($"╟Ваше первое число: {firstValue} ╢");
+                Console.WriteLine("╟═════════════════════╢");
+                Console.WriteLine($"╟Ваше второе число: {secondValue} ╢");
+                Console.WriteLine("╚═════════════════════╝");
+            }
+        }
+    }
+}
